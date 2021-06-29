b4576361ed110b973bb4dece51d304b3f393670e
sabrinawhite4 <sabrinawhite4@me.com>
2021-06-29 13:14:46 -0600

    adding rollbar

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require('express');
 const path = require('path');
 const gradient = require('gradient-string');
 const app = express();
+const RollBar = require('rollbar');
+const rollbar = new RollBar({
+    accessToken: 'ebd583141fbc4450a07084d54c962253',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+});
 
 app.use(express.json());
 
 app.get('/', (req,res) => {
+    rollbar.log("Hello world!");
 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-})
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {console.log(gradient.instagram(`Server rocking on ${port}`))});
