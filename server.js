06326435d76adfc553819da084d8427bc3b3bd39
sabrinawhite4 <sabrinawhite4@me.com>
2021-06-29 14:40:14 -0600

    initial commit

--- server.js
+++ server.js
@@ -9,16 +9,43 @@ const rollbar = new RollBar({
     captureUnhandledRejections: true
 });
 
+
 app.use(express.json());
 
-// app.get('/' student) => {
-    
-// }
+
+const animal = []
 
 app.get('/', (req,res) => {
     rollbar.log("Hello Sabrina!");
 
+    rollbar.warning('Unavailable')
+    rollbar.critical('App has crashed!')
     res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.post('/api/animal', (req, res) => {
+    let {animal} = req.body
+    animal = animal.trim();
+
+    const index = animals.findIndex((animalName) => {
+        animalName === animal
+    });
+
+    try {
+        if (index === -1 && name !== '') {
+            animals.push(animal)
+            rollbar.log('Animal added successfully', {author: 'Sabrina', type: 'manual'});
+            res.status(200).send(animals)
+        } else if (animal === '') {
+            rollbar.error('no animal provided');
+            res.status(400).send('must provide an animal')
+        } else {
+            rollbar.error('Animal already exists')
+            res.status(400).send('That animal already exists')
+        }
+    } catch (err) {
+        rollbar.error(err)
+    }
 });
 
 const port = process.env.PORT || 4000;
